fafe10b40f9e5447ab626a2dce301833b1937474
josericardopenase <josericardopenase@gmail.com>
2023-04-05 02:43:36 +0100

    finished tree data structure

--- src/collections/tree.rs
+++ src/collections/tree.rs
@@ -1,9 +1,11 @@
+use std::fmt;
+
 pub struct Node<T>{
     value : T,
     children : Vec<Node<T>>
 }
 
-impl<T> Node<T>{
+impl<T: fmt::Display> Node<T>{
     pub fn new(value : T) -> Node<T>
     {
         return Node{
@@ -19,14 +21,43 @@ impl<T> Node<T>{
     pub fn remove_node(&mut self, index : usize) -> (){
         self.children.remove(index);
     }
+
+    fn get_tree_string(&self, prefix: &str) -> String {
+        let mut tree_str = format!("{}{}\n", prefix, self.value);
+        let child_prefix = format!("{}{}", prefix, "│   ");
+        let last_child_prefix = format!("{}{}", prefix, "└── ");
+
+        let num_children = self.children.len();
+        for (i, child) in self.children.iter().enumerate() {
+            let child_prefix = if i == num_children - 1 {
+                last_child_prefix.clone()
+            } else {
+                child_prefix.clone()
+            };
+            tree_str += &format!("{}{}", child_prefix, child.get_tree_string(&prefix.replace("└──", "    ")));
+        }
+
+        return tree_str;
+    }
+
+    pub fn to_string(&self) -> String {
+        return self.get_tree_string("");
+    }
 }
 
 pub struct Tree<T>{
-    pub root : Node<T>
+    pub root : Option<Node<T>>
 }
 
-impl<T> Tree<T>{
-    pub fn new(&mut self, root : Node<T>){
-        self.root = root;
+impl<T: fmt::Display> Tree<T>{
+    pub fn new() -> Tree<T>{
+        return Tree::<T>{root : None};
+    }
+
+    pub fn to_string(&self) -> String {
+        match &self.root {
+            Some(node) => return node.to_string(),
+            None => return String::new(),
+        }
     }
 }
--- src/interpreter/parser/base.rs
+++ src/interpreter/parser/base.rs
@@ -1,14 +1,22 @@
-pub struct Parser{
+use std::fmt;
 
-    
+use crate::{collections::tree::Tree, interpreter::lexer::base::Token};
+
+struct SyntaxToken{
+}
+
+pub struct Parser{
+   ast : Tree<SyntaxToken> 
 }
 
 impl Parser{
-    pub fn new() -> Parser{
-        return Parser{};
+    pub fn new(){
+        /*return Parser{
+            ast: Tree::<SyntaxToken>::new()
+        };*/
     }
 
-    pub fn parse(&self, line : String){
+    pub fn parse(&self, tokens : Vec<Token>){
         
     }
 }
